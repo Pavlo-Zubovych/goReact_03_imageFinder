cfc41c0b6955479c79ca460333588e74b11501bf
PavloZubovych <zu10@ukr.net>
2022-07-10 14:38:24 +0300

    renamed

---
+++ src/components/LoadSpinner/LoadSpinner.js
@@ -0,0 +1,17 @@
+// import ThreeDots from 'react-loader-spinner';
+// import styles from './LoaderSpinner.module.css';
+
+// const LoaderSpinner = () => {
+//   return (
+//     <ThreeDots
+//       className={styles.Loader}
+//       type='ThreeDots'
+//       color='#000000'
+//       height={80}
+//       width={80}
+//       timeout={3000}
+//     />
+//   );
+// };
+
+// export default LoaderSpinner;
